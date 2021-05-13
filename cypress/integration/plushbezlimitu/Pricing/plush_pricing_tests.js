67d756fbbe813d233a5ffc6a7037fc1eb03cc577
Szymon Wasiak <wasiak.szymon.1991@gmail.com>
2021-05-13 14:46:06 +0200

    Back accept rodo function

--- cypress/integration/plushbezlimitu/Pricing/plush_pricing_tests.js
+++ cypress/integration/plushbezlimitu/Pricing/plush_pricing_tests.js
@@ -18,11 +18,11 @@ describe("Current price lists", () =>{
             plush_pricing_list_page.visitPricingListPage()
             plush_pricing_list_page.clearCookies()
 
-            // plush_pricing_list_page.acceptRodo()
+            plush_pricing_list_page.acceptRodo()
             plush_pricing_list_page.scrollIntoPricingPlace(data.pricingList[0])
     })
 
-    it("Price list for the provision of telecommunications services tariff 'PLUSH ABO III' (from 13.01.2021)",()=>{
+    it.only("Price list for the provision of telecommunications services tariff 'PLUSH ABO III' (from 13.01.2021)",()=>{
         plush_pricing_list_page.checkedFirstActualPricingDocument(data.pricingList[1],data.pricingList[2],data.pricingList[2], data.pricingList[3],data.urlPricingList[0],0)
     })
     it("Price list of international calls and international roaming 4 (from 13.01.2021)",()=>{
